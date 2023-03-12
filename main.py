02268e9d730eabadc33453c359fc6ca0f71ded5e
Simo <76773814+Hovari@users.noreply.github.com>
2023-03-12 15:36:16 +0200

    Update main.py
    updatet main

--- main.py
+++ main.py
@@ -1,56 +1,73 @@
-# Import necessary modules
-import machine
+from machine import ADC, Pin, I2C
+from ssd1306 import SSD1306_I2C
+import utime
+import socket
 import network
 import secrets
-import ssd1306
-import utime
-
-# Set up network connection
+# Network settings
 wlan = network.WLAN(network.STA_IF)
 wlan.active(True)
 wlan.connect(secrets.SSID, secrets.PASSWORD)
-while not wlan.isconnected():
+
+if wlan.isconnected() == False:
+    deviceIp ="No ip"    
     utime.sleep(1)
-deviceIp = wlan.ifconfig()[0]
-print("Connected to Wi-Fi. IP address:", deviceIp)
+        
+ipconfig = wlan.ifconfig()
+deviceIp = ipconfig[0]
+print("Connected to wifi" )    
 
-# Set up soil moisture sensor
-soil = machine.ADC(machine.Pin(26))
-moistureMin = 18700 # When wet
-moistureMax = 44074 # When dry
-print("ADC:", soil.read_u16())
+# Moisture sensor settings
+soil = ADC(Pin(26)) # Soil moisture PIN reference 
+# Calibration of capacitive soil sensor
+min=18700 #When wet
+max=44074 #When dry
+print("ADC: "+str(soil.read_u16()))
 
-# Set up OLED display
-displayWidth = 128
-displayHeight = 64
-i2c = machine.I2C(0, scl=machine.Pin(1), sda=machine.Pin(0), freq=200000)
-oled = ssd1306.SSD1306_I2C(displayWidth, displayHeight, i2c)
+# How often the information is updated
+# 1 sec = 1
+interval = 10
 
-# Main loop
+# Display settings
+width  = 128                                         
+height = 64                                    
+fill = 1
+nofill = 0
+theme = False #False or True, as u like
+              
+i2c = I2C(0, scl=Pin(1), sda=Pin(0), freq=200000)
+oled = SSD1306_I2C(width, height, i2c)    
+print("I2C Address      : "+hex(i2c.scan()[0]).upper()) # Display device address
+print("I2C Configuration: "+str(i2c))                   # Display I2C config
+print("")
+
 while True:
+
     oled.fill(0)
-    moisture = (moistureMax - soil.read_u16()) * 100 / (moistureMax - moistureMin)
-
-    # Display moisture level
-    oled.text("{:.0f}%".format(moisture), 92, 8)
-
-    # Display device IP address
-    oled.text(deviceIp, 12, 42)
-
-    # Display ADC value
-    oled.text("ADC: {}".format(soil.read_u16()), 12, 52)
-
-    # Display moisture level as bar graph
-    oled.rect(12, 18, 104, 20, 1) # Frame
-    oled.fill_rect(14, 20, int(moisture), 16, 1) # Filling
-
+    oled.invert(theme)
+    moisture = (max-soil.read_u16())*100/(max-min)
+           
+    # OLEDTXT   
+    oled.text("%.0f" % moisture +"%",92,8)
+    oled.text(deviceIp,12,42 )
+    oled.text("ADC:"+str(soil.read_u16()) ,12,52)
+    
+    # OLEDBAR
+    oled.rect(12, 18, 104, 20, fill) #frame
+    oled.fill_rect(14, 20, int(moisture), 16, fill)  #filling
+    
     oled.show()
-
-    # Print moisture level to console
+    
     if moisture > 40:
-        print("Moisture level: {}%".format(int(moisture)))
+        print("moist", int(moisture),"%")
     else:
-        print("Dry. Moisture level: {}%".format(int(moisture)))
+        print("dry", int(moisture),"%")
+    
+    utime.sleep(interval) 
+    
+    
+    
 
-    # Wait for specified interval before updating display and moisture level
-    utime.sleep(10)
+
+
+
